f8486586cc44c776fb50e02b505edb744cb25965
Luanfcc <165920001+Luanfcc@users.noreply.github.com>
2024-04-08 18:01:16 +0100

    Add files via upload

---
+++ Dados.js
@@ -0,0 +1,48 @@
+function rolard4(){
+    var randomNumber = Math.floor(Math.random() * 4) + 1;
+    var res = document.getElementById('res')
+    res.innerHTML = `${randomNumber}`
+
+}
+
+function rolard6(){
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    var res = document.getElementById('res')
+    res.innerHTML = `${randomNumber}`
+
+}
+
+function rolard8(){
+    var randomNumber = Math.floor(Math.random() * 8) + 1;
+    var res = document.getElementById('res')
+    res.innerHTML = `${randomNumber}`
+
+}
+
+function rolard10(){
+    var randomNumber = Math.floor(Math.random() * 10) + 1;
+    var res = document.getElementById('res')
+    res.innerHTML = `${randomNumber}`
+
+}
+
+function rolard12(){
+    var randomNumber = Math.floor(Math.random() * 12) + 1;
+    var res = document.getElementById('res')
+    res.innerHTML = `${randomNumber}`
+
+}
+
+function rolard20(){
+    var randomNumber = Math.floor(Math.random() * 20) + 1;
+    var res = document.getElementById('res')
+    res.innerHTML = `${randomNumber}`
+
+}
+
+function rolard100(){
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+    var res = document.getElementById('res')
+    res.innerHTML = `${randomNumber}`
+
+}
